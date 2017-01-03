dd2ae31ca54104e1b23f079f8761ed8001fd121b
Daniel McQuiston <daniel.pro@dehugo.net>
2017-01-03 09:19:00 -0700

    Add driver for V-Pedal

---
+++ src/lib/vpedal.js
@@ -0,0 +1,56 @@
+
+function button (digitalByteIndex, digitalByteMask) {
+	return buf => {
+		let output = {digital:false};
+
+		output.digital = buf[digitalByteIndex] === digitalByteMask;
+
+		return output;
+	};
+}
+
+const KEYS = {
+	BUTTON_REW: {
+		label: "Rewind",
+		type: "Button",
+		parseData: button(4, 0x01)
+	},
+	BUTTON_FWD: {
+		label: "Fast Forward",
+		type: "Button",
+		parseData: button(4, 0x02)
+	},
+	BUTTON_PLAY: {
+		label: "Play/Pause",
+		type: "Button",
+		parseData: button(4, 0x03)
+	},
+};
+
+const VPEDAL = {
+	VENDOR_ID: 0x04b4,
+	PRODUCT_ID: 0x5555,
+	KEYS: KEYS,
+	parseData: buf => {
+		let parsed = {};
+
+		if (buf) {
+			const keys = Object.keys(KEYS);
+			for (const i in keys) {
+				const name = keys[i];
+				const def = KEYS[name];
+
+				let buttonData = def.parseData(buf);
+				parsed[name] = {
+					label: def.label,
+					type: def.type,
+					digital: buttonData.digital
+				};
+			}
+		}
+
+		return parsed;
+	}
+};
+
+module.exports = VPEDAL;

80c72850b55407e298b7772c5dbf62cff0dd5524
Daniel McQuiston <daniel.pro@dehugo.net>
2017-01-03 09:19:19 -0700

    Add liteweight debug file

---
+++ debug.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const HID = require("node-hid");
+const robot = require("robotjs");
+const controller = require("./src/lib/vpedal");
+const events = require("./src/lib/events");
+
+const hid = new HID.HID(controller.VENDOR_ID, controller.PRODUCT_ID);
+
+hid.gotData = function (err, data) {
+	if (err) {
+		console.error(err);
+		process.exit();
+	} else if (!data) {
+		console.log("device disconnected");
+		process.exit();
+	}
+	let parsedData = controller.parseData(data);
+	console.log(parsedData)
+
+	this.read(this.gotData.bind(this));
+};
+
+hid.read(hid.gotData.bind(hid));

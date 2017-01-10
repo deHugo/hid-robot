ce27456953b78af96b0967f68b55f1c2342d9976
Daniel McQuiston <daniel.pro@dehugo.net>
2017-01-09 21:45:55 -0700

    Add debugger

--- debug.js
+++ debug.js
@@ -2,24 +2,47 @@
 
 const HID = require("node-hid");
 const robot = require("robotjs");
-const controller = require("./src/lib/infinity-pedal");
+const drivers = require("./src/lib/drivers");
 const events = require("./src/lib/events");
 
-const hid = new HID.HID(controller.VENDOR_ID, controller.PRODUCT_ID);
+for (let driverName in drivers) {
+	const driver = drivers[driverName];
 
-hid.gotData = function (err, data) {
-	if (err) {
-		console.error(err);
-		process.exit();
-	} else if (!data) {
-		console.log("device disconnected");
-		process.exit();
+	const devName = driver.PRODUCT_NAME||driver.PRODUCT_ID;
+	const devVendor = driver.VENDOR_NAME||driver.VENDOR_ID;
+
+	let gotDataErr = null;
+
+	try {
+		const device = new HID.HID(driver.VENDOR_ID, driver.PRODUCT_ID);
+
+		device.gotData = function (err, data) {
+			if (err) {
+				console.error(err);
+				gotDataErr = err;
+			} else if (!data) {
+				console.log(`Device '${devName}' disconnected.`);
+				gotDataErr = 'Device disconnected';
+			}
+
+			if (driver.parseData) {
+				let parsedData = driver.parseData(data);
+				console.log(parsedData);
+			} else {
+				console.log(data)
+			}
+
+			if (gotDataErr) {
+				this.pause();
+			} else {
+				this.read(this.gotData.bind(this));
+			}
+		};
+
+		device.read(device.gotData.bind(device));
+
+		console.log(`Device '${devName}' found and connected.`);
+	} catch (err) {
+		console.log(`Device '${devName}' by '${devVendor}' not found.`);
 	}
-	let parsedData = controller.parseData(data);
-	console.log(parsedData);
-	// console.log(data)
-
-	this.read(this.gotData.bind(this));
-};
-
-hid.read(hid.gotData.bind(hid));
+}

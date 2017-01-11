da0666693a51a8236fdfd3a8e542721417cba8b9
Daniel McQuiston <daniel.pro@dehugo.net>
2017-01-11 12:49:25 -0700

    Add device wrapper for drivers

---
+++ src/lib/devices.js
@@ -0,0 +1,76 @@
+"use strict";
+
+const EventEmitter = require("events");
+const HID = require("node-hid");
+const drivers = require("./drivers");
+
+let devices = {};
+
+for (let driverName in drivers) {
+	const driver = drivers[driverName];
+
+	devices[driverName] = {
+		getInputs: getInputs.bind(getInputs, driver),
+		listen: listen.bind(listen, driver),
+	};
+}
+
+function getInputs (driver) {
+	let inputs = {};
+
+	for (let key in driver.KEYS) {
+		inputs[key] = {
+			label:        driver.KEYS[key].label,
+			type:         driver.KEYS[key].type,
+			defaultValue: driver.KEYS[key].defaultValue,
+			valueRange:   driver.KEYS[key].valueRange,
+		};
+	}
+
+	return inputs;
+}
+
+function listen (driver) {
+	const devName = driver.PRODUCT_NAME||driver.PRODUCT_ID;
+	const devVendor = driver.VENDOR_NAME||driver.VENDOR_ID;
+
+	let gotDataErr = null;
+
+	let emitter = new EventEmitter();
+
+	let output = new Promise((resolve, reject) => {
+		try {
+			const device = new HID.HID(driver.VENDOR_ID, driver.PRODUCT_ID);
+
+			device.gotData = function (err, data) {
+				if (err) {
+					reject(err);
+					gotDataErr = err;
+				} else if (!data) {
+					emitter.emit("disconnect",`Device '${devName}' disconnected.`);
+					gotDataErr = "Device disconnected";
+				}
+
+				let parsedData = driver.parseData(data);
+
+				emitter.emit("data", parsedData);
+
+				if (gotDataErr) {
+					this.pause();
+				} else {
+					this.read(this.gotData.bind(this));
+				}
+			};
+
+			device.read(device.gotData.bind(device));
+
+			resolve({emitter, message:`Device '${devName}' found and connected.`});
+		} catch (err) {
+			reject(`Device '${devName}' by '${devVendor}' not found.`);
+		}
+	});
+
+	return output;
+}
+
+module.exports = devices;

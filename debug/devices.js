67c4eb7f8856fcbb15a52f261ac7b6e77b53d7f5
Daniel McQuiston <daniel.pro@dehugo.net>
2017-01-11 15:56:49 -0700

    Add another debug file

---
+++ debug/devices.js
@@ -0,0 +1,20 @@
+const devices = require("./src/lib/devices");
+
+console.log(devices.VPEDAL.getInputs());
+
+devices.INFINITY_PEDAL.listen()
+    .then(device => {
+        console.log(device.message)
+
+        device.emitter.on("data", data => console.log(data))
+        device.emitter.on("disconnect", disconnect => console.log(disconnect));
+    }, err => console.error(err))
+
+devices.VPEDAL.listen()
+    .then(device => {
+        console.log(device.message)
+
+        device.emitter.on("data", data => console.log(data))
+        device.emitter.on("disconnect", disconnect => console.log(disconnect));
+    }, err => console.error(err))
+    .catch(err => console.error(err));

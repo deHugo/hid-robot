8e6f116fe33c870e2595ed1bc376dd1bd5d60fd8
Daniel McQuiston <daniel.pro@dehugo.net>
2017-01-12 08:57:30 -0700

    Link debug file

--- debug/devices.js
+++ debug/devices.js
@@ -3,18 +3,18 @@ const devices = require("../src/lib/devices");
 console.log(devices.VPEDAL.getInputs());
 
 devices.INFINITY_PEDAL.listen()
-    .then(device => {
-        console.log(device.message)
+	.then(device => {
+	console.log(device.message);
 
-        device.emitter.on("data", data => console.log(data))
-        device.emitter.on("disconnect", disconnect => console.log(disconnect));
-    }, err => console.error(err))
+	device.emitter.on("data", data => console.log(data));
+	device.emitter.on("disconnect", disconnect => console.log(disconnect));
+}, err => console.error(err));
 
 devices.VPEDAL.listen()
-    .then(device => {
-        console.log(device.message)
+	.then(device => {
+	console.log(device.message);
 
-        device.emitter.on("data", data => console.log(data))
-        device.emitter.on("disconnect", disconnect => console.log(disconnect));
-    }, err => console.error(err))
-    .catch(err => console.error(err));
+	device.emitter.on("data", data => console.log(data));
+	device.emitter.on("disconnect", disconnect => console.log(disconnect));
+}, err => console.error(err))
+	.catch(err => console.error(err));
